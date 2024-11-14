d768ed01bc0ab0979bc2dac860b8dc3b0321fe7f
HIMANSHU KUMAR <himanshukumar74580@gmail.com>
2024-11-14 23:06:49 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+            Your one-stop destination for timeless fashion. Explore our exclusive collections crafted with care to bring you the best in style and quality. Shop with confidence, knowing that every piece is designed to last a lifetime. You can tweak it to better fit your branding tone!
+            
+            </p>
+            <b className='text-gray-800'>Developed By - Himanshu Pandey</b>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+1-212-456-7890</li>
+                <li>contact@himanshuproject07</li>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
